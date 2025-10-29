b19f675589a75a3dac29bcb791ab31ead61d62a5
OwlConductor <esser.lennart@smail.uni-koeln.de>
2025-10-29 15:09:10 +0100

    Modulo to minus

--- h1/H1_main.java
+++ h1/H1_main.java
@@ -6,13 +6,14 @@ public class H1_main {
     public static void main(String args[]) {
         Random random = new Random();
         int z = random.nextInt();
-
+        System.out.println(String.valueOf(z));
         /*
-         * Shrink value of z with modulo 10 operation to an risidual value below 10
+         * Shrink value of z minus 10
          */
         if(z>10)
-            z =  z % 10;
+            z =  z - 10;
 
         System.out.println(String.valueOf(z));
     }
+    
 }
